a727dfd5f8fffc982472541fb7de4c67fd095dd0
caiweipeng <caiweipeng@yeah.net>
2020-04-10 18:24:01 +0800

    [weipeng.cai]

--- src/components/MathCalulator/index.js
+++ src/components/MathCalulator/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Calculator from "../Calculator/Calculator";
 import withAverage from "../../HOC/withAverage/withAverage";
+import { EmailContext } from "../../context/profieContext";
 
 class Mathe extends React.Component {
   static subject = { code: "mathe", score: 0 };
+  static contextType = EmailContext;
+
+  componentWillUnmount() {
+    this.props._removeSubject(Mathe.subject)
+  }
+
   render() {
-  return <><span>{/*请用class.contextType的方式获取EmailContext的值*/}</span><Calculator cb={this.props._editScore} />{/*请用props render的方式渲染附加分数组件最好使用children属性*/}</>;
+    const email = this.context;
+    return (
+      <>
+        <span>
+          {/*请用class.contextType的方式获取EmailContext的值*/}
+          {email}
+        </span>
+        <Calculator cb={this.props._editScore} />
+        {/*请用props render的方式渲染附加分数组件最好使用children属性*/}
+        {this.props.children}
+      </>
+    )
+    
   }
 }
 export default withAverage(Mathe, Mathe.subject);
