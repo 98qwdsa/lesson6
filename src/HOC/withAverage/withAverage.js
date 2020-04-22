ce07343f017590bb9f43c52d1e59780f04dbd06f
william_lin <123@163.com>
2020-04-23 00:55:12 +0800

    homework done

--- src/HOC/withAverage/withAverage.js
+++ src/HOC/withAverage/withAverage.js
@@ -1,12 +1,20 @@
 import React from "react";
 import demoData from "../../demoData";
-// import hoistNonReactStatic from 'hoist-non-react-statics';
+import hoistNonReactStatic from "hoist-non-react-statics";
+import DataSource from "../../demoData";
 export default function withAverage(Warp, subject) {
   //Warp.prototype.componentWillUnmount = function() {};
   class Average extends React.Component {
     //实现在组件实例化的时候在模拟服务中添加改科目与成绩
+    componentDidMount() {
+      DataSource._addSubject(subject);
+    }
 
     //实现在组件销毁的时候在模拟服务中删除改科目与成绩
+    componentWillUnmount() {
+      DataSource._removeSubject(subject);
+    }
+
     _editScore(score) {
       demoData._editScore({ ...subject, ...{ score } });
     }
@@ -14,18 +22,13 @@ export default function withAverage(Warp, subject) {
       const { title, ...passThroughProps } = this.props;
       return (
         <>
-        <p>
-          {title}
-        </p>
-        <Warp
-          {...passThroughProps}
-          _editScore={this._editScore}
-        />
+          <p>{title}</p>
+          <Warp {...passThroughProps} _editScore={this._editScore} />
         </>
       );
     }
-  };
+  }
   //Average.subject = Warp.subject
-  //hoistNonReactStatic(Average, Warp);
+  hoistNonReactStatic(Average, Warp);
   return Average;
 }
--- src/demoData.js
+++ src/demoData.js
@@ -3,25 +3,26 @@ const DataSource = {
   _TotalSubject: [],
   _setAerage() {
     let _TotalScore = 0;
-    this._TotalSubject.forEach(e => {
+    this._TotalSubject.forEach((e) => {
       _TotalScore += e.score;
     });
     this._Average = (_TotalScore / this._TotalSubject.length).toFixed(2);
-    this.aerageChangeCb && this.aerageChangeCb(this._Average);
+    this.aerageChangeCb &&
+      this.aerageChangeCb(isNaN(this._Average) ? 0 : this._Average);
   },
   _addSubject(subject) {
     this._TotalSubject.push(subject);
     this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
   },
   _removeSubject(subject) {
-    this._TotalSubject = this._TotalSubject.filter(e => {
+    this._TotalSubject = this._TotalSubject.filter((e) => {
       return e.code !== subject.code;
     });
     this._setAerage();
     this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
   },
   _editScore(subject) {
-    this._TotalSubject = this._TotalSubject.map(e => {
+    this._TotalSubject = this._TotalSubject.map((e) => {
       if (e.code === subject.code) {
         return subject;
       }
@@ -31,7 +32,7 @@ const DataSource = {
     this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
   },
   aerageChangeCb: null,
-  subjectChangeCb: null
+  subjectChangeCb: null,
 };
 
 export default DataSource;
